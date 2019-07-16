5b00e55a7fda0de7ac8ac943edb7ead620391e61
Erica Hawkins <ehawk901@gmail.com>
2019-07-16 11:05:56 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,11 +2,12 @@ require_relative 'config/environment'
 require "pry"
 class App < Sinatra::Base
 
-  get '/food' do
+  get '/food_form' do
     erb :food_form
   end
  
   post '/food' do
+
     # params = { :name => " ",
     #            :favorite_food => " "}
     # params.to_s
